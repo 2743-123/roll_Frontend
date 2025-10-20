ee0abd1fe3d78c02249069b6a0ac64555ca0315c
asif <asifandhi01@gmail.com>
2025-10-20 11:41:40 +0530

    final deplay try

--- src/environment.config.ts
+++ src/environment.config.ts
@@ -2,19 +2,26 @@ type EnvConfig = {
   Base_API_URL: string;
 };
 
-const ENV = "local";
+const ENV = process.env.NODE_ENV || "local"; // auto detects development / production
 
 const ENVIRONMENT: Record<string, EnvConfig> = {
-  local: {Base_API_URL: process.env.REACT_APP_API_URL || "https://bricks-admin-backend.onrender.com/api/auth", },
+  local: {
+    Base_API_URL:
+      process.env.REACT_APP_API_URL || "http://localhost:5000/api/auth",
+  },
   development: {
-   Base_API_URL: process.env.REACT_APP_API_URL || "https://bricks-admin-backend.onrender.com/api/auth",
+    Base_API_URL:
+      process.env.REACT_APP_API_URL ||
+      "https://bricks-admin-backend.onrender.com/api/auth",
   },
   production: {
-    Base_API_URL: process.env.REACT_APP_API_URL || "https://bricks-admin-backend.onrender.com/api/auth",
+    Base_API_URL:
+      process.env.REACT_APP_API_URL ||
+      "https://bricks-admin-backend.onrender.com/api/auth",
   },
 };
 
-const ENVIRONMENT_VARIABLES: EnvConfig = ENVIRONMENT[ENV];
+const ENVIRONMENT_VARIABLES: EnvConfig =
+  ENVIRONMENT[ENV as keyof typeof ENVIRONMENT] || ENVIRONMENT.local;
 
 export default ENVIRONMENT_VARIABLES;
-//  "http://localhost:5000/api/auth"
