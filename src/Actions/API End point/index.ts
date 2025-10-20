22e71259b46126b6ca13d7d053611e159ef8f521
asif <asifandhi01@gmail.com>
2025-10-20 11:12:26 +0530

    third

--- src/Actions/API End point/index.ts
+++ src/Actions/API End point/index.ts
@@ -1,5 +1,6 @@
 import ENVIRONMENT_VARIABLES from "../../environment.config";
 
+
 export const API_AUTH_LOGIN = `${ENVIRONMENT_VARIABLES.Base_API_URL}/login`;
 export const API_AUTH_LOGOUT = `${ENVIRONMENT_VARIABLES.Base_API_URL}/logout`;
 export const API_USER_GET = `${ENVIRONMENT_VARIABLES.Base_API_URL}/users`;
@@ -13,3 +14,5 @@ export const API_CREATE_TOKEN = `${ENVIRONMENT_VARIABLES.Base_API_URL}/create`;
 export const API_UPDATE_TOKEN = `${ENVIRONMENT_VARIABLES.Base_API_URL}/update`;
 export const API_CONFIRM_TOKEN = `${ENVIRONMENT_VARIABLES.Base_API_URL}/confirm
 `;
+
+
