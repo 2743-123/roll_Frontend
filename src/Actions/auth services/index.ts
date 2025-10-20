a245dc9178c1da3298964ddce87d603b79d755e6
asif <asifandhi01@gmail.com>
2025-10-20 13:43:46 +0530

    24

--- src/Actions/auth services/index.ts
+++ src/Actions/auth services/index.ts
@@ -3,7 +3,7 @@ import { API_AUTH_LOGIN, API_AUTH_LOGOUT } from "../API End point";
 import { getAccessToken } from "../../AuthToekn";
 
 export const loginUser = async (email: string, password: string) => {
-  const { data } = await axios.post(API_AUTH_LOGIN, { email, password });
+  const { data } = await axios.post(API_AUTH_LOGIN, { email, password },{ withCredentials: true });
   return data;
 };
 
